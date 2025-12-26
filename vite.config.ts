d96c5cf1690f368efa09a75cdf7ce9b0408b23b3
Giacomo Costantino <JackCosta2612@users.noreply.github.com>
2025-12-26 15:47:41 +0100

    Fix base path for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/data_analysis_portf/',
   plugins: [react()],
-  base: '/data_analysis_portf/'
 })
